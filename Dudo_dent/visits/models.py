069ed9d879a17d87ca225df97904eb6febd6631b
branimiranastasov <branimir.anastassov@gmail.com>
2025-05-05 15:19:42 +0300

    feat: added urls, models, views and html files

--- Dudo_dent/visits/models.py
+++ Dudo_dent/visits/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+from Dudo_dent.procedures.models import Procedure
+from Dudo_dent.patients.models import Patient
+
 # Create your models here.
+
+class Visit(models.Model):
+    date = models.DateField()
+    patient = models.ForeignKey(
+        to=Patient,
+        on_delete=models.CASCADE,
+        related_name='visits',
+    )
+
+    procedure = models.ManyToManyField(
+        Procedure,
+        related_name='visits',
+    )
+
+    def __str__(self):
+        return f"{self.date}-{self.patient.full_name}"
