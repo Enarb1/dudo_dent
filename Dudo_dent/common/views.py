789e89837a3bf41c235ed91166b029ddb94c6402
branimiranastasov <branimir.anastassov@gmail.com>
2025-05-21 16:45:40 +0300

    feat:added searchfield on the home page

--- Dudo_dent/common/views.py
+++ Dudo_dent/common/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from Dudo_dent.patients.forms import SearchPatientForm
+from Dudo_dent.patients.models import Patient
+
+
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'common/home-page.html')
+    form = SearchPatientForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'common/home-page.html', context)
