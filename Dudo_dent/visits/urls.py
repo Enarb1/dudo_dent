77ec52a15ca8e76c992fa3675d15d5de47da9a84
branimiranastasov <branimir.anastassov@gmail.com>
2025-05-21 17:38:34 +0300

    feat: added edit and delete logic for the visits

--- Dudo_dent/visits/urls.py
+++ Dudo_dent/visits/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from Dudo_dent.visits import views
 urlpatterns = [
     path('', views.all_visits, name='all-visits'),
-    path('<int:pk>/', views.visit_by_id, name='visit-details'),
+    path('<int:pk>/', include([
+        path('', views.visit_by_id, name='visit-details'),
+        path('edit-visit/', views.edit_visit, name='edit-visit'),
+        path('delete-visit/', views.delete_visit, name='delete-visit'),
+
+    ])),
     path('add-visit/', views.add_visit, name='add-visit'),
 ]
