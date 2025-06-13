96eb13a7f3dd4248f968109156040af96dddd731
branimiranastasov <branimir.anastassov@gmail.com>
2025-06-13 17:25:42 +0300

    changed all functions views in patients views to CBVs

--- Dudo_dent/patients/views.py
+++ Dudo_dent/patients/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 
 from Dudo_dent.patients.forms import PatientCreateForm, PatientEditForm, SearchPatientForm
 from Dudo_dent.patients.models import Patient
@@ -8,35 +8,44 @@ from Dudo_dent.patients.models import Patient
 
 # Create your views here.
 
-def all_patients(request):
+class AllPatientsView(ListView):
+    model = Patient
+    template_name = 'patients/patients-main.html'
+    form_class = SearchPatientForm
+    query_param = 'query'
 
-    patients = Patient.objects.all().order_by('full_name')
-    form = SearchPatientForm(request.GET)
+    def get_context_data(self, *, object_list =None, **kwargs):
+        kwargs.update({
+            'form': self.form_class(),
+            'query': self.request.GET.get(self.query_param,'')
+        })
+        
+        return super().get_context_data(object_list=object_list,**kwargs)
 
-    if request.method == 'GET':
-        if form.is_valid():
-            query = form.cleaned_data['query']
-            patients = patients.filter(full_name__icontains=query).order_by('full_name')
+    def get_queryset(self):
+        queryset = self.model.objects.all()
+        search_value = self.request.GET.get(self.query_param)
 
+        if search_value:
+            queryset = queryset.filter(
+                full_name__icontains=search_value,
+            )
 
-    context = {
-        'patients': patients,
-        'form': form
-    }
+        return queryset.order_by('full_name')
 
-    return render(request, 'patients/patients-main.html', context)
+class PatientDetailsView(DetailView):
+    model = Patient
+    template_name = 'patients/patient-details.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'patient_slug'
 
+    def get_context_data(self,**kwargs):
+        visits = self.object.visits.all().order_by('-date')
+        kwargs.update({
+            'visits':visits
+        })
 
-def patient_details(request, patient_slug: str):
-    patient = Patient.objects.filter(slug=patient_slug).first()
-    patient_visits = patient.visits.all().order_by('-date')
-
-    context = {
-        'patient': patient,
-        'patient_visits': patient_visits
-    }
-
-    return render(request, 'patients/patient-details.html', context)
+        return super().get_context_data(**kwargs)
 
 
 class PatientCreateView(CreateView):
@@ -56,25 +65,6 @@ class PatientCreateView(CreateView):
         context['return_to'] = self.request.GET.get('return_to') or self.request.POST.get('return_to')
         return context
 
-
-# def add_patient(request):
-#     form = PatientCreateForm(request.POST or None)
-#     return_to = request.GET.get('return_to') or request.POST.get('return_to')
-#
-#     if request.method == 'POST' and form.is_valid():
-#         form.save()
-#
-#         if return_to == 'add-visit':
-#             return redirect('add-visit')
-#         return redirect('all-patients')
-#
-#     context = {
-#         'form': form,
-#         'return_to': return_to
-#     }
-#
-#     return render(request, 'patients/add-patient.html', context)
-
 class EditPatientView(UpdateView):
     model = Patient
     form_class = PatientEditForm
@@ -92,29 +82,6 @@ class EditPatientView(UpdateView):
         context['patient'] = self.object
         return context
 
-
-
-# def edit_patient(request, patient_slug: str):
-#     patient = Patient.objects.filter(slug=patient_slug).first()
-#
-#     if request.method == 'POST':
-#         form = PatientEditForm(request.POST,instance=patient)
-#
-#         if form.is_valid():
-#             form.save()
-#
-#             return redirect('patient-details', patient_slug=patient.slug)
-#     else:
-#         form = PatientEditForm(instance=patient)
-#
-#     context = {
-#         'form': form,
-#         'patient': patient
-#     }
-#
-#     return render(request,'patients/edit-patient.html', context)
-
-
 class DeletePatientView(DeleteView):
     # with conformation form
     model = Patient
@@ -123,23 +90,6 @@ class DeletePatientView(DeleteView):
     slug_url_kwarg = 'patient_slug'
     success_url = reverse_lazy('all-patients')
 
-
-    # without conformation form
-    # def post(self, request,*args, **kwargs):
-    #     patient = get_object_or_404(Patient, slug=self.kwargs['patient_slug'])
-    #     patient.delete()
-    #     return redirect('all-patients',)
-    #
-    # def get(self, *args, **kwargs):
-    #     return redirect('patient-details', patient_slug=self.kwargs['patient_slug'])
-#
-# def delete_patient(request, patient_slug: str):
-#     patient = get_object_or_404(Patient, slug=patient_slug)
-#     if request.method == 'POST':
-#         patient.delete()
-#         return redirect('all-patients')
-#
-#     return redirect('patient-details', patient_slug=patient.slug)
 
 
 
