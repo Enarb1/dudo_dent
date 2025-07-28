36a8230ce9c84c31f23072c39e3196002991a734
branimiranastasov <branimir.anastassov@gmail.com>
2025-07-28 15:39:46 +0300

    feat: reset password logic addded

--- Dudo_dent/accounts/urls.py
+++ Dudo_dent/accounts/urls.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, \
+    PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path, include
 
 from Dudo_dent.accounts import views
@@ -7,6 +8,13 @@ urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/',PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+
     path('all-accounts/', views.AccountsListView.as_view(), name='all-accounts'),
     path('<int:pk>/', include([
         path('', views.UserProfileView.as_view(), name='profile'),
@@ -17,3 +25,6 @@ urlpatterns = [
     path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done')
 
 ]
+
+
+
