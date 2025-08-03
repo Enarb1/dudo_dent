ca00beb94134fc2f8dadd889ab48a1e8e5a71af4
branimiranastasov <branimir.anastassov@gmail.com>
2025-08-03 17:39:27 +0300

    entrypoint.sh updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Starting Celery worker..."
+celery -A Dudo_dent worker --loglevel=info &
+
 echo "Starting Gunicorn server..."
 exec gunicorn Dudo_dent.wsgi:application --bind 0.0.0.0:8000
