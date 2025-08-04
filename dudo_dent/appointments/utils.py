ce0c504084ab86e0bf31bc4080f73e2104cce323
branimiranastasov <branimir.anastassov@gmail.com>
2025-08-04 21:38:35 +0300

    fix: GoogleService Account info

--- dudo_dent/appointments/utils.py
+++ dudo_dent/appointments/utils.py
@@ -1,3 +1,4 @@
+import json
 from datetime import date, timedelta, datetime
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -12,8 +13,9 @@ def get_calendar_service():
     """
     Getting the Google Calendar service.
     """
-    credentials = service_account.Credentials.from_service_account_file(
-        settings.GOOGLE_SERVICE_ACCOUNT_INFO,
+
+    credentials = service_account.Credentials.from_service_account_info(
+        json.loads(settings.GOOGLE_SERVICE_ACCOUNT_INFO),
         scopes=['https://www.googleapis.com/auth/calendar'],
     )
     service = build('calendar', 'v3', credentials=credentials)
